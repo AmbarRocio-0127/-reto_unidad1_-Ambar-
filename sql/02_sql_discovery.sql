eb35c434f810b23b4efe364fca7b7f6785bbc258
Ambar <ambarcaraballo.r@gmail.com>
2026-06-23 21:35:13 -0400

    Analisis a TechVentas S.A.S

--- sql/02_sql_discovery.sql
+++ sql/02_sql_discovery.sql
@@ -3,5 +3,10 @@ SELECT DISTINCT producto AS Producto FROM ventas_techventas;
 SELECT * FROM ventas_techventas WHERE fecha BETWEEN '2024-01-01' AND '2024-03-31' AND Cantidad > 5;
 
 SELECT vendedor,  SUM(Cantidad * precio_unitario) AS Ingreso_Bruto FROM ventas_techventas
-GROUP BY vendedor HAVING SUM(Cantidad * precio_unitario) ORDER BY vendedor;
+GROUP BY vendedor HAVING SUM(Cantidad * precio_unitario) > 10000 ORDER BY vendedor;
 
+SELECT COUNT(Categoria) FROM ventas_techventas;
+
+SELECT SUM(cantidad) from ventas_techventas;
+
+SELECT AVG(precio_unitario) FROM ventas_techventas;
